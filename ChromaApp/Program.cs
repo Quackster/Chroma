faba40317f2af754533a5617230db92e7d36f119
Quackster <quacksterr.15@gmail.com>
2020-12-12 02:55:10 +1000

    Update Program.cs

--- ChromaApp/Program.cs
+++ ChromaApp/Program.cs
@@ -10,12 +10,12 @@ namespace ChromaApp
         {
             ChromaFurniture furni;
 
-            furni = new ChromaFurniture(@"C:\Users\Alex\Documents\GitHub\Havana\tools\www\r38\dcr\hof_furni\tv_flat.swf", isSmallFurni: false, renderState: 0, renderDirection: 2);
+            furni = new ChromaFurniture("tv_flat.swf", isSmallFurni: false, renderState: 0, renderDirection: 2);
             furni.Run();
 
             File.WriteAllBytes(furni.GetFileName(), furni.CreateImage());
 
-            furni = new ChromaFurniture(@"C:\Users\Alex\Documents\GitHub\Havana\tools\www\r38\dcr\hof_furni\chair_silo.swf", isSmallFurni: false, renderState: 0, renderDirection: 2, colourId: 2);
+            furni = new ChromaFurniture("chair_silo.swf", isSmallFurni: false, renderState: 0, renderDirection: 2, colourId: 2);
             furni.Run();
 
             File.WriteAllBytes(furni.GetFileName(), furni.CreateImage());
