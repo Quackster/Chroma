820711bc194b747cea0eaa64d3dc4ee3480eeda2
Quackster <quacksterr.15@gmail.com>
2020-12-20 13:24:55 +1000

    Update compile.bat

--- ChromaWebApp/compile.bat
+++ ChromaWebApp/compile.bat
@@ -1,1 +1,2 @@
 dotnet publish --self-contained -c Release -r linux-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
+dotnet publish --self-contained -c Release -r win-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
